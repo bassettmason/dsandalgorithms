a5904debe18cba45e4ea9f9c679c157d496afc38
Mason Bassett <bassettmason@gmail.com>
2018-12-10 09:44:29 -0800

    start of lab

---
+++ src/main/java/stacksandqueues/Node.java
@@ -0,0 +1,13 @@
+package stacksandqueues;
+
+public class Node {
+
+    public int data;
+    public linkedlist.Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
