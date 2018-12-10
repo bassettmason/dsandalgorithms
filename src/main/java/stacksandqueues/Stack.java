f5f87563bb9afc218a1b8ad40500ea4bfefc9692
Mason Bassett <bassettmason@gmail.com>
2018-12-10 10:32:52 -0800

    gradle

--- src/main/java/stacksandqueues/Stack.java
+++ src/main/java/stacksandqueues/Stack.java
@@ -1,4 +1,25 @@
 package stacksandqueues;
 
 public class Stack {
+    public Node top;
+//    Stack stack = new Stack();
+    public Stack() {
+
+
+        this.top = null;
+
+        public static void push (int value){
+            Node newNode = new Node(value);
+            if (top == null){
+                top = newNode;
+            }else {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+
+    }
+
 }
+
+
