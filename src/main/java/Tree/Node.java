2bad0c3a9a80a9210c1a05f0e811f5b639066bd2
mason bassett <bassettmason@gmail.com>
2018-12-17 13:55:45 -0800

    not_done

---
+++ src/main/java/Tree/Node.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class Node {
+
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+}
